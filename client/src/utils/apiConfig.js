a249a31382b8b3a5f8c962f0e0e1b8de0c9a64da
weezy1456 <w800548@gmail.com>
2024-07-30 15:54:56 +0100

    update

--- client/src/utils/apiConfig.js
+++ client/src/utils/apiConfig.js
@@ -1,6 +1,6 @@
 
-// export const api ='https://biteapinew.onrender.com';
-export const api ='http://localhost:5000';
+export const api ='https://biteapinew.onrender.com';
+// export const api ='http://localhost:5000';
 
 
 
